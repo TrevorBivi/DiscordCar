d515dd3ce232e184b6c9c8189a6f48649e47b245
Ehren Julien-Neitzert <wakydawgster@gmail.com>
2019-02-17 14:33:51 +0000

    now goes forward AND BACKWARDS

--- gpio_test.py
+++ gpio_test.py
@@ -6,13 +6,24 @@ import discord
 import asyncio
 
 
+class PwmWad:
+
+    def __init__(self, pins, freq = 100):
+        gpio.setup(pins, gpio.OUT)
+        self._pwms = [gpio.PWM(p, freq) for p in pins] #create all the pwms, make them all the same frequency
+        for pwm in self._pwms:
+            pwm.start(0) #start each pwm at 0 duty
+    
+    def change_duty(self, new_duty):
+        for pwm in self._pwms:
+            pwm.ChangeDutyCycle(new_duty)
+
+
 bot = commands.Bot(command_prefix = "!", case_insensitive = False)
 
 gpio.setmode(gpio.BOARD) #use BOARD pin numbering
-OUT_PIN = 19
-gpio.setup(OUT_PIN, gpio.OUT)
-PWM = gpio.PWM(OUT_PIN, 3) #3 hz
-PWM.start(0)
+OUT_PINS = (19, 21, 23, 24)
+PWM = PwmWad(OUT_PINS)
 
 cam = PiCamera()
 cam.start_preview()
@@ -20,17 +31,22 @@ time.sleep(2) #HAVE TO SLEEP FOR 2 SECS TO LET THE CAMERA "WARM UP" (yeah I know
 
 
 @bot.command()
-async def on(ctx, *args):
-    PWM.ChangeDutyCycle(50) #50% duty
-    await ctx.send("gpio on hopefully")
+async def forward(ctx, *args):
+    PWM.change_duty(50)
+    await ctx.send("going forward hopefully")
 
 @bot.command()
-async def off(ctx, *args):
-    PWM.ChangeDutyCycle(0)
-    await ctx.send("gpio off hopefully")
+async def backward(ctx, *args):
+    PWM.change_duty(5)
+    await ctx.send("going backwards hopefully")
 
 @bot.command()
 async def stop(ctx, *args):
+    PWM.change_duty(0)
+    await ctx.send("not moving hopefully")
+
+@bot.command()
+async def end(ctx, *args):
     PWM.stop()
     gpio.cleanup()
     msg = await ctx.send("gpio stopped hopefully")
@@ -40,16 +56,6 @@ def take_pic():
     with open("test.jpg", "wb") as img:
         cam.capture(img)
     return "test.jpg"
-
-#@bot.command()
-#async def snap(ctx, *args):
-#    img_name = take_pic()
-#    msg = await ctx.send(file = discord.File(img_name, filename=img_name))
-#    await ctx.send("gonna edit...")
-#    img_name = take_pic
-#    await msg.delete()
-#    await ctx.send("edited")
-#    await ctx.send("took picture hopefully")
 
 async def img_spam():
     print('background process started')
